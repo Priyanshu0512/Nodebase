22f3e677007cc9649c3ceb7b2451026c4b4a9900
Priyanshu0512 <priyanshuk0595@gmail.com>
2026-01-08 23:46:19 +0530

    Better auth setup and protected procedure added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import { Client } from "./client";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
+import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
 
 const page = async () => {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+  await requireAuth();
+
+  const data = await caller.getUsers();
+
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<p>loading...</p>}>
-        <Client />
-      </Suspense>
-    </HydrationBoundary>
+    <div>
+      hi
+      {JSON.stringify(data)}
+    </div>
   );
 };
 
