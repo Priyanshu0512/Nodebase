cdbf0997cb21a96f99bc4a355f0e56bda7a919cd
Priyanshu0512 <priyanshuk0595@gmail.com>
2026-01-27 03:45:33 +0530

    Added Editor page & edit workflow name.

--- src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
+++ src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
@@ -1,4 +1,14 @@
+import { prefetchWorkflow } from "@/features/workflows/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+import {
+  Editor,
+  EditorError,
+  EditorLoading,
+} from "@/features/editor/components/editor";
+import { EditorHeader } from "@/features/editor/components/editor-header";
 
 interface PageProps {
   params: Promise<{ workflowId: string }>;
@@ -7,7 +17,18 @@ interface PageProps {
 const page = async ({ params }: PageProps) => {
   await requireAuth();
   const { workflowId } = await params;
-  return <div>Workflow Id: {workflowId}</div>;
+  prefetchWorkflow(workflowId);
+  return (
+    <HydrateClient>
+      <ErrorBoundary fallback={<EditorError />}>
+        <Suspense fallback={<EditorLoading />}></Suspense>
+        <EditorHeader workflowId={workflowId} />
+        <main className="flex-1">
+          <Editor workflowId={workflowId} />
+        </main>
+      </ErrorBoundary>
+    </HydrateClient>
+  );
 };
 
 export default page;
